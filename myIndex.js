d16945ce5532be5e6c4ddf2d56f944fa40713562
christianrevillard <christian.revillard@centraliens.net>
2013-12-21 17:29:33 +0100

    0.9

--- myIndex.js
+++ myIndex.js
@@ -7,17 +7,3 @@ handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 server.start(router.route, handle);
-
-/**
-	// This is working fine !
-var server = require("./openShiftServer");
-var http = require("http");
-
-function onRequest(request, response) {
-	 response.writeHead(200, {"Content-Type": "text/html"});
- response.write("coucou");
- response.end();
- };
-
-http.createServer(onRequest).listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
-**/
