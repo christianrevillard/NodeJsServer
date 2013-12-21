ac9eabfb37b5214606721f625f8ae841e9d89f3c
christianrevillard <christian.revillard@centraliens.net>
2013-12-21 16:45:48 +0100

    0.9

--- myIndex.js
+++ myIndex.js
@@ -9,7 +9,6 @@ handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 server.start(router.route, handle);
 //console.log("Testing");
-**/
 
 //  OpenShift sample Node application
 var express = require('express');
@@ -18,3 +17,14 @@ var serv = express.createServer();
 
 serv.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP, function() {
             console.log('log');});
+
+**/
+			
+var http = require("http");
+function onRequest(request, response) {
+	 response.writeHead(200, {"Content-Type": "text/html"});
+ response.write("coucou");
+ response.end();
+ };
+
+http.createServer(onRequest).listen(process.env.OPENSHIFT_NODEJS_PORT);
