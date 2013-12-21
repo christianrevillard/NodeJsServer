e0b09d0582a9840b7f583f8f761bb06f1d8f1559
christianrevillard <christian.revillard@centraliens.net>
2013-12-21 16:13:55 +0100

    0.9

--- myIndex.js
+++ myIndex.js
@@ -3,11 +3,10 @@
 var server = require("./openShiftserver");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
-**/
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
-/**
 server.start(router.route, handle);
 **/
+console.log("Testing");
