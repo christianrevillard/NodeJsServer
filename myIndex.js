187e5d684ce1008da216397903bab81cee282c77
christianrevillard <christian.revillard@centraliens.net>
2013-12-21 17:37:09 +0100

    0.9

--- myIndex.js
+++ myIndex.js
@@ -1,5 +1,5 @@
 #!/bin/env node
-var server = require("./openShiftServer");
+var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 var handle = {}
--- server.js
+++ server.js
@@ -19,8 +19,9 @@ function onRequest(request, response) {
  route(handle, pathname, response, postData);
  });
 }
-
-	http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(
+		process.env.OPENSHIFT_NODEJS_PORT || 8888, 
+		process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 	console.log("Server has started.");}
 
 exports.start = start;
