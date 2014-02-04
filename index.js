12a5a5bbda42d77a31d111648d048d82ea0774f3
christianrevillard <christian.revillard@centraliens.net>
2014-02-04 21:55:33 +0100

    stuff

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var server = require("./ServerCore/server");
 var router = require("./ServerCore/router");
 
 var uploadStartHandler = require("./PageHandlers/upload/startHandler");
+var uploadStartHandlerTest = require("./PageHandlers/startHandler");
 var uploadShowHandler = require("./PageHandlers/upload/showHandler");
 var uploadHandler = require("./PageHandlers/upload/uploadHandler");
 var menuHandler = require("./PageHandlers/menuHandler");
@@ -9,7 +10,7 @@ var menuHandler = require("./PageHandlers/menuHandler");
 var handlers = {};
 handlers["/"] = menuHandler.handle;
 handlers["/upload/"] = uploadStartHandler.handle;
-handlers["/upload/start"] = uploadStartHandler.handle;
+handlers["/upload/start"] = uploadStartHandlerTest.handle;
 handlers["/upload/upload"] = uploadHandler.handle;
 handlers["/upload/show"] = uploadShowHandler.handle;
 
