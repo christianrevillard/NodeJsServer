64059593d7867b5f4d2ef7713637cb24afa53d63
christianrevillard <christian.revillard@centraliens.net>
2014-10-29 09:43:27 +0100

    fixed at last?

--- resources/js/collisionClient.js
+++ resources/js/collisionClient.js
@@ -2,8 +2,8 @@ var CreCollision = CreCollision || {};
 
 CreCollision.onload = function ()
 {		
-	// openshift websocket on 8000 or secure on 8443
-	var socket = io("/collision", {port:8000});
+	// openshift websocket on 8000 or secure on 8443 - must configurable !!!	
+	var socket = io("http://nodejs-creweb.rhcloud.com:8000/collision");
 	 		
 	var theCanvas = document.getElementById('theCanvas');
 
