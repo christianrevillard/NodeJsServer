48e6244a4706192fc13306e73ed762b891f52a47
christianrevillard <christian.revillard@centraliens.net>
2014-11-01 23:49:50 +0100

    big reorganisation

--- CreanvasModule/Decorators/Droppable.js
+++
@@ -1,8 +0,0 @@
-var applyTo = function(element, droppableData)
-{
-	element.isDroppable = true;
-	element.dropZone = droppableData["dropZone"];
-	element.ondrop = droppableData["ondrop"];
-}
-
-exports.applyTo = applyTo;
--- CreanvasModule/Vector.js
+++
@@ -1,7 +0,0 @@
-var Vector = function(x, y, z){
-	this.x = x;
-	this.y = y;
-	this.z = z;	
-};
-
-exports.Vector = Vector;
